c53274d967faccbf919e4370107a741009381d30
trueChazza <chaz.goodare@gmail.com>
2023-02-25 22:40:54 +1300

    build: add details

--- mix.exs
+++ mix.exs
@@ -7,6 +7,11 @@ defmodule Extaima.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: "Simple duration parser - HH:MM:SS, H:MM:SS, MM:SS, M:SS",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/midarrlabs/extaima"}
+      ],
       deps: deps()
     ]
   end
